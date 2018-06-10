72d5a921b008e2c5c36a6b92ab5838c5337e7109
Evgeny Belkin <sunafajro@gmail.com>
2018-06-11 00:24:13 +0300

    мелкие изменения

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <div>
-    <nav-component :notify="notify" v-if="!isGuest" />
+  <div class="wrapper">
+    <nav-component :notify="notify" v-if="!userIsGuest" />
     <b-container fluid>
-      <bc-component :path="getPath" v-if="!isGuest" />
+      <bc-component :path="getPath" v-if="!userIsGuest" />
       <router-view :notify="notify" />
     </b-container>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import bContainer from "bootstrap-vue/es/components/layout/container";
 import Navigation from "./components/NavBar/Navigation";
 import Breadcrumbs from "./components/Breadcrumbs/Breadcrumbs.vue";
@@ -21,18 +22,16 @@ export default {
     }
   },
   computed: {
+    ...mapGetters(["userIsGuest"]),
     getPath() {
       const path = this.$route.path.split("/");
       path[0] = "home";
       return path;
-    },
-    isGuest() {
-      return this.$store.getters.isGuest;
     }
   },
   components: {
-    "bc-component": Breadcrumbs,
     "b-container": bContainer,
+    "bc-component": Breadcrumbs,
     "nav-component": Navigation
   }
 };
