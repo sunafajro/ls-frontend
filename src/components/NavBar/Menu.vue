c450f13b7499d028268aba4958a6a7db74c790d6
Evgeny Belkin <sunafajro@gmail.com>
2018-06-10 17:26:22 +0300

    частично переработан список ссылок меню навигации

--- src/components/NavBar/Menu.vue
+++ src/components/NavBar/Menu.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="top-nav-collapse" class="collapse navbar-collapse">
+  <b-collapse is-nav id="nav_collapse">
     <ul id="nav-links" class="navbar-nav mr-auto">
       <li class="nav-item" v-if="loading">
         <span class="navbar-text">Загружаем панель навигации...</span>
       </li>
       <li class="nav-item" v-for="link in links" :key="link.id">
-        <a class="nav-link" :href="link.url" :title="link.title"><span :class="link.classes"></span></a>
+        <router-link  class="nav-link" :title="link.title" :to="link.url"><span :class="link.classes"></span></router-link>
       </li>
       <li class="nav-item"><a class="nav-link" href="#" @click.stop="handleLogout"><span aria-hidden="true" class="fa fa-sign-out" title="Выход"></span></a></li>
     </ul>
-  </div>
+  </b-collapse>
 </template>
 
 <script>
+import bCollapse from "bootstrap-vue/es/components/collapse/collapse";
+
 export default {
   props: {
     links: {
@@ -23,6 +25,9 @@ export default {
       type: Boolean,
       required: true
     }
+  },
+  components: {
+    "b-collapse": bCollapse
   },
   methods: {
     handleLogout() {
