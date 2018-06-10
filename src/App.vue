6ec9417b84e2cc31919ec07dc239750ed97b00d6
Evgeny Belkin <sunafajro@gmail.com>
2018-06-10 17:25:36 +0300

    добавлен breadcrumbs для авторизованных пользователей

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <nav-component v-if="!isGuest" :notify="notify"/>
+    <nav-component :notify="notify" v-if="!isGuest" />
     <b-container fluid>
+      <bc-component :path="getPath" v-if="!isGuest" />
       <router-view :notify="notify" />
     </b-container>
   </div>
@@ -10,6 +11,7 @@
 <script>
 import bContainer from "bootstrap-vue/es/components/layout/container";
 import Navigation from "./components/NavBar/Navigation";
+import Breadcrumbs from "./components/Breadcrumbs/Breadcrumbs.vue";
 
 export default {
   props: {
@@ -19,11 +21,17 @@ export default {
     }
   },
   computed: {
+    getPath() {
+      const path = this.$route.path.split("/");
+      path[0] = "home";
+      return path;
+    },
     isGuest() {
       return this.$store.getters.isGuest;
     }
   },
   components: {
+    "bc-component": Breadcrumbs,
     "b-container": bContainer,
     "nav-component": Navigation
   }
