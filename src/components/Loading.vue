51a3dedcea9aa904f516f39dc5d0b0d927f11595
Evgeny Belkin <sunafajro@gmail.com>
2018-06-11 00:25:03 +0300

    алерт обернут в контейнер

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,12 +1,17 @@
 <template>
-  <b-alert show variant="warning">Загрузка приложения...</b-alert>
+  <b-container fluid>
+    <b-alert show variant="warning">Загрузка приложения...</b-alert>
+  </b-container>
 </template>
 
 <script>
 import bAlert from "bootstrap-vue/es/components/alert/alert";
+import bContainer from "bootstrap-vue/es/components/layout/container";
+
 export default {
   components: {
-    "b-alert": bAlert
+    "b-alert": bAlert,
+    "b-container": bContainer
   }
 };
 </script>
