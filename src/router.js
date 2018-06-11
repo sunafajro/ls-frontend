fd67c8e3b3d1fba1ca2c22e00193b5458381359f
Evgeny Belkin <sunafajro@gmail.com>
2018-06-11 17:36:13 +0300

    добален маршрут для раздела сообщений

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./components/Home.vue";
 import Login from "./components/Login.vue";
-import Messages from "./components/Messages/List.vue";
+import Messages from "./components/Messages/App.vue";
 
 Vue.use(Router);
 
