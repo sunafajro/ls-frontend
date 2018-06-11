b7701b3d488d2025d9ed61419ee9858bfbaede52
Evgeny Belkin <sunafajro@gmail.com>
2018-06-11 17:35:09 +0300

    добавлен store для сообщений с методами

---
+++ src/store/messages.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { notify } from "../utils";
+
+export const messagesModule = {
+  state: {
+    access: {
+      create: false,
+      update: false,
+      delete: false
+    },
+    messages: [],
+    targets: []
+  },
+  getters: {
+    getAccess(state) {
+      return state.access;
+    },
+    getMessages(state) {
+      return state.messages;
+    },
+    getTargets(state) {
+      return state.targets;
+    }
+  },
+  mutations: {
+    updateMessages(state, data) {
+      state.access = data.access;
+      state.messages = data.messages;
+    },
+    updateTargets(state, data) {
+      state.targets = data.targets;
+    }
+  },
+  actions: {
+    getMessages({ commit }, payload) {
+      const { type, fromDate, toDate } = payload;
+      const url = `/message/get-messages?type=${type ? type : ""}&from=${
+        fromDate ? fromDate : ""
+      }&to=${toDate ? toDate : ""}`;
+      axios(url)
+        .then(response => {
+          if (response.data.status) {
+            commit("updateMessages", {
+              access: response.data.actions,
+              messages: response.data.messages
+            });
+          } else {
+            notify("error", response.data.text);
+          }
+        })
+        .catch(err => {
+          notify("error", err.message ? err.message : "Произошла ошибка!");
+        });
+    },
+    getTargets({ commit }) {
+      axios("/message/get-targets")
+        .then(response => {
+          if (response.data.status) {
+            commit("updateTargets", { targets: response.data.targets });
+          } else {
+            notify("error", response.data.text);
+          }
+        })
+        .catch(err => {
+          notify("error", err.message ? err.message : "Произошла ошибка!");
+        });
+    }
+  }
+};
