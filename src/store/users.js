ed532153e804786f10850837b2ee2f7310770729
Evgeny Belkin <sunafajro@gmail.com>
2018-06-11 17:34:49 +0300

    добавлен пустой store для пользователей

---
+++ src/store/users.js
@@ -0,0 +1,16 @@
+//import axios from "axios";
+//import { notify } from "../utils";
+
+export const usersModule = {
+  state: {
+    users: []
+  },
+  getters: {},
+  mutations: {},
+  actions: {
+    getUsers() {},
+    createUser() {},
+    updateUser() {},
+    deleteUser() {}
+  }
+};
