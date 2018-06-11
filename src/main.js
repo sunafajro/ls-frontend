6f962b6859327456ec713c9571741649243eb89e
Evgeny Belkin <sunafajro@gmail.com>
2018-06-11 17:36:54 +0300

    мелкие изменения

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
-import Noty from "noty";
 import { mapActions } from "vuex";
 import { mapGetters } from "vuex";
 import App from "./App.vue";
 import Loading from "./components/Loading.vue";
 import router from "./router";
 import store from "./store";
+import { notify } from "./utils";
 import "font-awesome/css/font-awesome.css";
 import "noty/lib/themes/bootstrap-v4.css";
 import "noty/lib/noty.css";
@@ -37,15 +37,7 @@ new Vue({
   },
   methods: {
     ...mapActions(["getUserState"]),
-    notify(type, text) {
-      new Noty({
-        theme: "bootstrap-v4",
-        text: text,
-        type: type,
-        timeout: 3000,
-        progressBar: false
-      }).show();
-    }
+    notify
   },
   render(h) {
     if (this.appLoaded) {
