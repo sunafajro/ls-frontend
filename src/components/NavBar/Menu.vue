c4e93dced4b04cc785454162e759176ec4168d5e
Evgeny Belkin <sunafajro@gmail.com>
2018-06-12 21:55:34 +0300

    мелкие исправления

--- src/components/NavBar/Menu.vue
+++ src/components/NavBar/Menu.vue
@@ -5,7 +5,7 @@
         <span class="navbar-text">Загружаем панель навигации...</span>
       </b-nav-text>
       <b-nav-item v-for="link in links" :key="link.id" v-if="!loading">
-        <router-link  class="nav-link" :title="link.title" :to="link.url"><span :class="link.classes"></span></router-link>
+        <router-link  class="nav-link" :title="link.title" :to="link.url"><span aria-hidden="true" :class="link.classes"></span></router-link>
       </b-nav-item>
       <b-nav-item><a class="nav-link" href="#" @click.stop="getLogout" v-if="!loading"><span aria-hidden="true" class="fa fa-sign-out" title="Выход"></span></a></b-nav-item>
     </b-navbar-nav>
