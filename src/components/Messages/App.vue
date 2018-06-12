82e16a5001e22424918817d6af4ec66fb3bed720
Evgeny Belkin <sunafajro@gmail.com>
2018-06-12 21:54:58 +0300

    блок информации о пользователе и кнопка создания, выведены в раздел сообщений

--- src/components/Messages/App.vue
+++ src/components/Messages/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <div id="content" class="col-sm-10">
-    <list-component :messages="messages" />
-  </div>
+  <b-row>
+    <b-col sm="12" md="2" lg="2" xl="2">
+      <info-component :notify="notify" />
+      <div v-if="access.create">
+        <h4>Действия:</h4>
+        <b-button variant="success" @click="openModal">
+          <span class="fa fa-plus" aria-hidden="true"></span> Добавить
+        </b-button>
+      </div>
+    </b-col>
+    <b-col sm="10" md="2" lg="2" xl="2">
+      <list-component :messages="messages" />
+    </b-col>
+  </b-row>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 import moment from "moment";
-import List from "./List";
+import bButton from "bootstrap-vue/es/components/button/button";
+import bCol from "bootstrap-vue/es/components/layout/col";
+import bRow from "bootstrap-vue/es/components/layout/row";
+import Info from "../Info.vue";
+import List from "./List.vue";
 
 moment.locale("ru");
 
@@ -43,13 +58,17 @@ export default {
     this.getTargets();
   },
   components: {
+    "b-button": bButton,
+    "b-col": bCol,
+    "b-row": bRow,
     //"filter-component": Filter,
-    //"info-component": Info,
+    "info-component": Info,
     "list-component": List
     //"modal-component": Modal
   },
   methods: {
-    ...mapActions(["getMessages", "getTargets"])
+    ...mapActions(["getMessages", "getTargets"]),
+    openModal() {}
   }
 };
 </script>
