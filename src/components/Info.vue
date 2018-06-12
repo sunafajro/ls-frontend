58207695e7ba8c4dc204a6088161ed544c576f89
Evgeny Belkin <sunafajro@gmail.com>
2018-06-12 21:54:03 +0300

    добавлен компонент с блоком информации о пользователе

---
+++ src/components/Info.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-card>
+    <small>{{userInfo.userName}}</small><br />
+    <small>{{userInfo.role.name}}</small>
+  </b-card>
+</template>
+
+<script>
+import bCard from "bootstrap-vue/es/components/card/card";
+import { mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters(["userInfo"])
+  },
+  components: {
+    "b-card": bCard
+  }
+}
+</script>
