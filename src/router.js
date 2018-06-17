6d986098eb23ec613d43658f0fea7f0213b40c8d
Evgeny Belkin <sunafajro@gmail.com>
2018-06-17 20:10:34 +0300

    маршрут / перенаправляется в news

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./components/Home.vue";
 import Login from "./components/Login.vue";
 import Messages from "./components/Messages/App.vue";
 
@@ -10,9 +9,7 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home,
-      props: true
+      redirect: "/news"
     },
     {
       path: "/login",
